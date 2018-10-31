e0d86e5d2a1b8c5afd7219ce11d242ebfc0706d8
JFrost135y <33869660+JFrost135y@users.noreply.github.com>
2018-10-31 11:28:01 -0400

    Add project files.

---
+++ Pair Programming Using Collections/Employee.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pair_Programming_Using_Collections
+{
+    public class Employee
+    {
+        public int EmployeeID { get; set; }
+        public string EmployeeName { get; set; }
+        public double RateofPay { get; set; }
+        public double HoursWorked { get; set; }
+
+        public Employee(int EmpID, string EmpName, double ROPay, double Hours)
+        {
+            EmployeeID = EmpID;
+            EmployeeName = EmpName;
+            RateofPay = ROPay;
+            HoursWorked = Hours;
+        }
+        public static double RegularPayTotal = 0;
+        public static double OverTimeTotal = 0;
+        public static double Grandtotal = 0;
+
+
+        public double calculateRegPay()
+        {
+            if (HoursWorked <= 40)
+            {
+                double RegPay = (HoursWorked * RateofPay);
+                return RegPay;
+            }
+            else
+            {
+                double RegPay = (40 * RateofPay);
+                return RegPay;
+            }
+        }
+        public double calculateOvertime()
+        {
+            if (HoursWorked > 40)
+            {
+                
+                double Overtime = (HoursWorked - 40) * (RateofPay * 1.5);
+                return Overtime;
+            }
+            else
+            {
+                double Overtime = 0;
+                return Overtime;
+            }
+        }
+
+
+        public double calculatepay(double RateofPay, double HoursWorked)
+        {
+            
+            
+
+                if (HoursWorked <= 40)
+                {
+                    double RegPay = RateofPay * HoursWorked;
+                    //Console.WriteLine($"\nEmployee Worked: {HoursWorked} and needs to get paid {RegPay:C}");
+                    Grandtotal += RegPay;
+                    RegularPayTotal += RegPay;
+                    return RegPay;
+
+                }
+
+                else
+                {
+                    //calculating Employees who worked more than 40 hours
+                    double OverPay = (calculateOvertime() + (40 * RateofPay));
+                    //Console.WriteLine($"\nEmployee Worked: {HoursWorked} and needs to get paid {OverPay:C}");
+                    OverTimeTotal += calculateOvertime();
+                    RegularPayTotal += calculateRegPay();
+                    Grandtotal += OverPay;
+                    
+                    return OverPay;
+                }
+               
+            
+        }
+
+    }
+}
---
+++ Pair Programming Using Collections/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Pair_Programming_Using_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee(223445, "John Landry", 15.00, 35.5));
+            employees.Add(new Employee(223476, "Angela Rush", 15.00, 32.25));
+            employees.Add(new Employee(223487, "Bill Marsh", 16.50, 45.5));
+            employees.Add(new Employee(223504, "Peggy Simon", 16.75, 50));
+            employees.Add(new Employee(223512, "Carl Yost", 15.75, 40));
+            employees.Add(new Employee(223519, "Joe Miles", 15.00, 40));
+            employees.Add(new Employee(223525, "Joan Jeffries", 14.25, 25.75));
+            employees.Add(new Employee(223536, "George Mills", 13.65, 38));
+            employees.Add(new Employee(223542, "Gary Cooper", 15.35, 37.25));
+            employees.Add(new Employee(223558, "Mary Millicent", 15.75, 40));
+
+            
+
+            Console.WriteLine($"EmployeeID:     Employee Name:        Rate of Pay:     Hours Worked:            Regular Pay:           Overtime Pay:         Pay Total:");
+
+            foreach (var item in employees)
+            {
+               
+                Console.WriteLine
+                    (
+                    $"{item.EmployeeID,10 }"           +          
+                    $"{item.EmployeeName, 18}"         +    
+                    $"{item.RateofPay, 18:C}"          +     
+                    $"{item.HoursWorked, 22}"          +   
+                    $"{item.calculateRegPay(), 20:C}"  + 
+                    $"{item.calculateOvertime(), 23:C}"+ 
+                    $"{item.calculatepay(item.RateofPay, item.HoursWorked),22:C}"
+                    );
+                
+            }
+            Console.WriteLine($"\nRegular pay total:{Employee.RegularPayTotal:C}\n");
+            Console.WriteLine($"\nOvertime total:{Employee.OverTimeTotal:C}\n");
+            Console.WriteLine($"\nGrandtotal:{Employee.Grandtotal:C}\n");
+        }
+
+
+   
+  
+    }
+}
